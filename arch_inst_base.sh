69fbf9bd0995a903831c4f50e81c61735763bbae
andrewhills <andrewhills@users.noreply.github.com>
2017-12-08 00:28:32 +1100

    modified volume control

--- arch_inst_base.sh
+++ arch_inst_base.sh
@@ -65,6 +65,7 @@ function install_os2() {
 	packages+=" ntfs-3g fuse-exfat exfat-utils dosfstools"
 	packages+=" samba"
 	packages+=" acpid"
+    #packages+=" alsa-plugins ladspa swh-plugins"
 	#packages+=" avahi nss-mdns"
     #packages+=" archlinux-keyring"
 	    
@@ -95,11 +96,16 @@ function install_os2() {
 	#setup_avahi
 	setup_memory_limit
 	setup_aur_script
-	#disable_coredump
 	setup_acpi
 	setup_autologin
 	setup_resizeramdisk
 	setup_misc_scripts
+	#disable_coredump
+    #setup_pulse_audio_eql
+}
+
+function setup_pulse_audio_eql() {
+    echo -e 'pcm.eq {\n\ttype ladspa\n\n\t#slave.pcm "plughw:0,0"\n\tslave.pcm "plug:dmix"\n\n\t#path "/usr/lib/ladspa"\n\n\tplugins [\n\t{\n\t\tlabel mbeq\n\t\tid 1197\n\t\tinput {\n\t\t\t# bands: 50hz, 100hz, 156hz, 220hz, 311hz, 440hz, 622hz, 880hz, 1250hz, 1750hz, 25000hz, 50000hz, 10000hz, 20000hz\n\t\t\tcontrols [ -5 -5 -5 -5 -5 -10 -20 -15 -10 -10 -10 -10 -10 -3 -2 ]\n\t\t\t}\n\t\t}\n\t]\n}\n\npcm.!default {\n\ttype plug\n\tslave.pcm "eq"\n}\n\n\npcm.dsp0 {\n\ttype plug\n\tslave.pcm "eq"\n}' > /etc/asound.conf
 }
 
 function setup_misc_scripts() {
@@ -382,7 +388,7 @@ function setup_acpi() {
 	
 	echo -e '#!/bin/bash\n\nb="/sys/class/backlight/*/brightness"\nv=$(cat $(ls $b | head -n 1))\nd=$(ls $b | head -n 1)\n\nif [ "$1"x == "up"x ]; then\n\techo "$(($v + 1))" > "$d"\nfi\n\nif [ "$1"x == "down"x ]; then\n\techo "$(($v - 1))" > "$d"\nfi\n' > /etc/acpi/actions/brightness.sh
 	
-	echo -e '#!/bin/bash\n\nexport PULSE_RUNTIME_PATH=`find /run/user/*/ -name pulse | head -n 1`\nuid=$(basename $(dirname $PULSE_RUNTIME_PATH))\nuname=`getent passwd "$uid" | cut -d: -f1`\n\nif [ "$1"x == "up"x ]; then\n\tsu $uname -c "amixer -q set Master 5%+ unmute"\nfi\n\nif [ "$1"x == "down"x ]; then\n\tsu $uname -c "amixer -q set Master 5%- unmute"\nfi\n\nif [ "$1"x == "mute"x ]; then\n\tsu $uname -c "amixer -q set Master toggle"\nfi\n' > /etc/acpi/actions/volume.sh
+	echo -e '#!/bin/bash\n\nexport PULSE_RUNTIME_PATH=`find /run/user/*/ -name pulse | head -n 1`\nuid=$(basename $(dirname $PULSE_RUNTIME_PATH))\nuname=`getent passwd "$uid" | cut -d: -f1`\n\nif [ "$1"x == "up"x ]; then\n\t#su $uname -c "amixer -D pulse -q set Master 5%+ unmute"\n\tsu $uname -c "pactl set-sink-mute @DEFAULT_SINK@ false"\n\tsu $uname -c "pactl set-sink-volume @DEFAULT_SINK@ +5%"\nfi\n\nif [ "$1"x == "down"x ]; then\n\t#su $uname -c "amixer -D pulse -q set Master 5%- unmute"\n \tsu $uname -c "pactl set-sink-mute @DEFAULT_SINK@ false"\n\tsu $uname -c "pactl set-sink-volume @DEFAULT_SINK@ -5%"\nfi\n\nif [ "$1"x == "mute"x ]; then\n\t#su $uname -c "amixer -D pulse -q set Master toggle"\n\tsu $uname -c "pactl set-sink-mute @DEFAULT_SINK@ toggle"\nfi\n' > /etc/acpi/actions/volume.sh
 	
 	chmod +rx /etc/acpi/actions/brightness.sh /etc/acpi/actions/volume.sh
 	
