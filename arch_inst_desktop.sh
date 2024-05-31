bd3133a21aac8ed6960a319c8153a8e93a98d216
ahillss <5051345+ahillss@users.noreply.github.com>
2024-05-31 23:11:50 +1000

    Update arch_inst_desktop.sh

--- arch_inst_desktop.sh
+++ arch_inst_desktop.sh
@@ -128,7 +128,7 @@ function setup_thunar() {
 
 function setup_vlc() {
 	mkdir -p $HOME/.config/vlc
-	echo -e "[qt4]\nqt-recentplay=0\nqt-privacy-ask=0\n\n[core]\nvideo-title-show=0\nplay-and-exit=1\none-instance-when-started-from-file=0\nsnapshot-path=$HOME/Pictures\nsnapshot-prefix=$N_[$T]_\nsnapshot-sequential=1\nkey-vol-up=Ctrl+Up\nkey-vol-down=Ctrl+Down\nkey-vol-mute=m\nstats=0\nstereo-mode=1" > $HOME/.config/vlc/vlcrc
+	echo -e "[qt4]\nqt-recentplay=0\nqt-privacy-ask=0\n\n[core]\nvideo-title-show=0\nplay-and-exit=1\none-instance-when-started-from-file=0\nsnapshot-path=$HOME/Pictures\nsnapshot-prefix=$N_[$T]_\nsnapshot-sequential=1\nkey-vol-up=Ctrl+Up\nkey-vol-down=Ctrl+Down\nkey-vol-mute=m\nkey-stop=\nkey-snapshot=s\nstats=0\nstereo-mode=1" > $HOME/.config/vlc/vlcrc
 	echo -e '[MainWindow]\nstatus-bar-visible=true' > $HOME/.config/vlc/vlc-qt-interface.conf
 }
 
